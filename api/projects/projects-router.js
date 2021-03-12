fbc60e03a0118793a923f9db1f7cca2bf8044a00
WeiWu2 <weidiwu207@gmail.com>
2021-03-12 03:51:03 -0500

    action router done

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,11 @@
-// Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+const projectsMiddleWare = require('./projects-middleware')
+const router = express.Router();
+
+
+
+
+
+
+module.exports = router
